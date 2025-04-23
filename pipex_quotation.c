aef6204adb696fe2e0a5248008280bb4217763bb
azh4rrr <150307571+azh4rrr@users.noreply.github.com>
2025-04-23 17:46:38 +0000

    d

--- pipex_quotation.c
+++ pipex_quotation.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex_quotation.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: azmakhlo <azmakhlo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 16:33:19 by azmakhlo          #+#    #+#             */
-/*   Updated: 2025/03/17 16:43:36 by azmakhlo         ###   ########.fr       */
+/*   Updated: 2025/04/23 17:07:36 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	single_quote(const char *s, size_t *i, size_t *start)
 	{
 		(*i)++;
 		(*start)++;
-		while (s[*i] && s[*i] != '\'')
+		while (s[*i] && s[*i] != '\"')
 			(*i)++;
 	}
 }
